5f2df2f7acee270df9ff433d42e914eceb80f7c9
Pieter Wuille <pieter.wuille@gmail.com>
2013-07-26 01:06:01 +0200

    Add support for watch-only addresses
    Changes:
    * Add Add/Have WatchOnly methods to CKeyStore, and implementations
      in CBasicKeyStore.
    * Add similar methods to CWallet, and support entries for it in
      CWalletDB.
    * Make IsMine in script/wallet return a new enum 'isminetype',
      rather than a boolean. This allows distinguishing between
      spendable and unspendable coins.
    * Add a field fSpendable to COutput (GetAvailableCoins' return type).
    * Mark watchonly coins in listunspent as 'watchonly': true.
    * Add 'watchonly' to validateaddress, suppressing script/pubkey/...
      in this case.
    
    Based on a patch by Eric Lombrozo.
    
    Backported to 0.8.x from 2eb96d15a6de7d655d5d87b3b48afd6982bcc325
    
    Conflicts:
    	src/bitcoinrpc.cpp

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -260,6 +260,7 @@ static const CRPCCommand vRPCCommands[] =
     { "enforcecheckpoint",      &enforcecheckpoint,      true,      false,		false },
     { "makekeypair",            &makekeypair,            true,     	false,		false },
     { "sendalert",              &sendalert,              true,      false,		false },
+    { "importaddress",          &importaddress,          false,     false,      true },
     { "listunspent",            &listunspent,            false,     false,      true },
     { "getrawtransaction",      &getrawtransaction,      false,     false,      false },
     { "createrawtransaction",   &createrawtransaction,   false,     false,      false },
@@ -1206,6 +1207,7 @@ Array RPCConvertValues(const std::string &strMethod, const std::vector<std::stri
     if (strMethod == "importprivkey"          && n > 2) ConvertTo<bool>(params[2]);
     if (strMethod == "verifychain"            && n > 0) ConvertTo<boost::int64_t>(params[0]);
     if (strMethod == "verifychain"            && n > 1) ConvertTo<boost::int64_t>(params[1]);
+    if (strMethod == "importaddress"          && n > 2) ConvertTo<bool>(params[2]);
 
     return params;
 }
